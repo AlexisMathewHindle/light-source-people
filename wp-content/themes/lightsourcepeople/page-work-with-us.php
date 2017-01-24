52be1e27e380086f13c35dc5ca6c4d9aae580769
Alexis Hindle <alexis@geni-design.com>
2017-01-24 17:57:24 +0000

    Image change work with us

--- wp-content/themes/lightsourcepeople/page-work-with-us.php
+++ wp-content/themes/lightsourcepeople/page-work-with-us.php
@@ -83,7 +83,7 @@ If you want to be a part of our dynamic growing company and add value to its cus
 
         </div>
         <div class="col-md-6">
-            <img src="<?php echo get_template_directory_uri() ?>/images/wordcloud.png" class="img-responsive">
+            <img src="<?php echo get_template_directory_uri() ?>/images/wordcloud.jpg" class="img-responsive">
         </div>
     </div>
 </div>
