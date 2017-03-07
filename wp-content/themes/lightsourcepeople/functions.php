7acd6e42baee407aef26ad9c5ed997784cb61766
Chris Ijoyah <chrisijoyah@gmail.com>
2017-03-07 14:50:30 +0000

    Updated Google API key.

--- wp-content/themes/lightsourcepeople/functions.php
+++ wp-content/themes/lightsourcepeople/functions.php
@@ -3,7 +3,7 @@
 function lightsourcepeople_scripts() {
     wp_enqueue_script( 'j-query', get_template_directory_uri() . '/js/lib/jquery-3.1.1.min.js' );
     wp_enqueue_script( 'app', get_template_directory_uri() . '/js/app.js', 'j-query');
-    wp_enqueue_script( 'googlemaps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDgMkLpl0X07nJuwhpRsn1c3I8DSRHm34A');
+    wp_enqueue_script( 'googlemaps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC1BqlTPpIcpAWOXs564kig0jeFmpC_CM4');
     wp_enqueue_style( 'lightsourcepeople-style', get_stylesheet_uri() );
     wp_enqueue_script( 'slick', 'https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js');    
 }
